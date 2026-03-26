27c1d13bd458dd896017a776bebcfd927ee2957e
NatanElseif <164780448+natanaelferreira123321@users.noreply.github.com>
2026-03-26 20:40:21 -0300

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,10 @@
+# calculadora.py
+def somar(a, b):
+    return a + b
+
+# Um teste simples que a pipeline vai rodar
+if __name__ == "__main__":
+    resultado = somar(2, 3)
+    print(f"Testando soma: 2 + 3 = {resultado}")
+    assert resultado == 5, "Erro: A soma deveria ser 5!"
+    print("Teste passou com sucesso!")
